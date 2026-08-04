bfbfaf02e74cdf390b258e57ce3a44d3c16651fd
thewanderingwizard <camandfre@gmail.com>
2026-08-03 20:25:40 -0700

    Preview blackletter footer type

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Cormorant_Garamond, DM_Sans } from "next/font/google";
+import { Cormorant_Garamond, DM_Sans, UnifrakturCook } from "next/font/google";
 import "./globals.css";
 
 const display = Cormorant_Garamond({
@@ -11,6 +11,12 @@ const sans = DM_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
   weight: ["400", "500", "600", "700"],
+});
+
+const blackletter = UnifrakturCook({
+  subsets: ["latin"],
+  variable: "--font-blackletter",
+  weight: "700",
 });
 
 export const metadata = {
@@ -31,7 +37,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${display.variable} ${sans.variable}`}>{children}</body>
+      <body className={`${display.variable} ${sans.variable} ${blackletter.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
